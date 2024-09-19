93180197db2d7ffae0ffa4646422cb301ef6b6b5
Diana Guadalupe Funes Díaz <20001034@alumnos.utleon.edu.mx>
2024-09-18 20:59:16 -0600

    Primer commit Miercoles 18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component({//forman un acomponente
   selector: 'app-root',
+  // template:`
+  // <div class='container'>
+  //   <div class='row'>
+  //     <div class='col-md-12'>
+  //       <h1>Angular</h1>
+  //     </div>
+
+  //   </div>
+  // </div>
+  // `,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'baseAngular';
+
+  dobleNumero(x:number):number{
+    return x*2;
+  }
+
+  alumno={
+    nombre: 'Jessi',
+    edad: 32,
+    curso:'Angular',
+    fechaInscrito: new Date(),
+    pago: 2500
+  }
 }
