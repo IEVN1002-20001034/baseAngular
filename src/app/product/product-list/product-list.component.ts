61b15227670d0584a9d62f134b4c0e2ba88d4585
Diana Guadalupe Funes Díaz <20001034@alumnos.utleon.edu.mx>
2024-09-21 10:08:45 -0600

    Sabado 21 Septiembre Formulario 1

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProductos } from '../productos';
 
 @Component({
   selector: 'app-product-list',
@@ -9,15 +10,24 @@ export class ProductListComponent {
 
   //*ngIf=true, o false Si hay productos que se muestre la tabla, si no, la tabla no se muestra.
 
-productos:any[]=[
+imageWidth:number=120;
+imageMargin:number=4;
+muestraImg:boolean=true;
+listFilter:string='';
+
+mustraImg():void{
+  this.muestraImg=!this.muestraImg;
+}
+
+productos: IProductos[]=[//arreglo de objetos
   {
     "productoId":1,
     "Modelo":"Sentra",
     "Descripcion":"4 puertas",
     "Precio":2000,
-    "Year":2023,
+    "Year": "Marzo 20 2023",
     "Marca":"NISSAN",
-    "Color":"Azul",
+    "Color":"Morado",
     "ImagenUrl":"https://img.freepik.com/vector-premium/coche-vehiculo-kawaii-vector-caracteres_840347-816.jpg"
   },
   {
@@ -25,9 +35,9 @@ productos:any[]=[
     "Modelo":"A4",
     "Descripcion":"4 puertas",
     "Precio":3000,
-    "Year":2022,
+    "Year": "Junio 23 2022",
     "Marca":"AUDI",
-    "Color":"Blanco",
+    "Color":"Azul",
     "ImagenUrl":"https://www.creativefabrica.com/wp-content/uploads/2022/12/31/Kawaii-Chibi-Car-Graphic-55500846-1.png"
   },
   {
@@ -35,9 +45,9 @@ productos:any[]=[
     "Modelo":"Rio",
     "Descripcion":"4 puertas",
     "Precio":15000,
-    "Year":2020,
+    "Year": "Octubre 01 2020",
     "Marca":"KIA",
-    "Color":"Rojo",
+    "Color":"Rosa",
     "ImagenUrl":"https://m.media-amazon.com/images/I/61hkuRaoAsL.jpg"
   }
 ]
---
+++ src/app/product/productos.ts
@@ -0,0 +1,12 @@
+export interface IProductos {
+
+    productoId:number,
+    Modelo:String;
+    Descripcion:string,
+    Precio: number,
+    Year:string,
+    Marca:string,
+    Color:string;
+    ImagenUrl: string;
+
+}
